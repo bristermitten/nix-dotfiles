89c65aecb3ab4e739dfff8f8dbe8bb89b15f9958
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-11-20 11:54:52 -0500

    add discord with openasar

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,13 +1,11 @@
 { pkgs, home, environment, ... }:
 {
- environment.systemPackages = with pkgs; [
-    (discord.override {
+ 
+
+  home.packages = [
+    (pkgs.discord.override {
       withOpenASAR = true;
       # withVencord = true; # can do this here too
     })
-  ]; 
-
-  home.packages = [
-    pkgs.discord
     ];
 }
