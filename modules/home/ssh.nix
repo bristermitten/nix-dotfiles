a3912008ccae595fde84544fe540984c846a073a
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-11-20 13:05:43 -0500

    add ssh config

---
+++ modules/home/ssh.nix
@@ -0,0 +1,27 @@
+{...}: {
+  programs.ssh = {
+    enable = true;
+    includes = ["config.d/*"];
+    forwardAgent = true;
+    extraConfig = ''
+          AddKeysToAgent yes
+          UseKeychain yes
+
+      Host github-uni
+              HostName github.com
+              User fi22264
+              IdentityFile ~/.ssh/id_ed25519_uni
+              IdentitiesOnly=yes
+
+      Host lab
+              HostName rd-mvb-linuxlab.bristol.ac.uk
+              ProxyJump seis
+
+      Host seis
+            HostName seis.bris.ac.uk
+            User fi22264
+
+
+    '';
+  };
+}
