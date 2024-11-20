4fd5efc55c07362e2ade5ff5c9a5938df5db0f7e
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-11-20 13:05:53 -0500

    improve git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.shellAliases = {
     g = "git";
@@ -15,9 +15,29 @@
       aliases = {
         ci = "commit";
       };
+
+signing = {
+        key = "~/.ssh/id_ed25519";
+      signByDefault = true;
+};
+
       extraConfig = {
         # pull.rebase = "false";
+        credential.helper =
+          if pkgs.stdenvNoCC.isDarwin
+          then "osxkeychain"
+          else "cache --timeout=1000000000";
+
+        commit.gpgsign = true;
+        gpg.format = "ssh";
+
+        init.defaultBranch = "main";
+        pull.rebase = true;
       };
+
+
+
+      difftastic.enable = true;
     };
     lazygit.enable = true;
   };
