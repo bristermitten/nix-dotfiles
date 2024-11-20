2dae6dfbdde517ddc006e671ba1e5443cafc6c14
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-11-20 12:44:34 -0500

    format discord.nix

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,11 +1,11 @@
 { pkgs, home, environment, ... }:
 {
- 
+
 
   home.packages = [
     (pkgs.discord.override {
       withOpenASAR = true;
       # withVencord = true; # can do this here too
     })
-    ];
+  ];
 }
