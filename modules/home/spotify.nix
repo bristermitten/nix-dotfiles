01986c438357558b17a03abb3f72b47ca23afc93
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-12-07 10:14:47 -0500

    setup spicetify

--- modules/home/spotify.nix
+++ modules/home/spotify.nix
@@ -1,8 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, flake, ... }:
 {
+  imports = [ 
+ flake.inputs.spicetify-nix.homeManagerModules.default ];
+
+   programs.spicetify =
+    let
+      spicePkgs = flake.inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+    {
+      enable = true;
+      #spicetifyPackage = pkgs.spicetify-cli;
+      #spotifyPackage = pkgs.spotify;
+      enabledExtensions = with spicePkgs.extensions; [ shuffle powerBar skipStats lastfm betterGenres songStats ];
+
+      theme = spicePkgs.themes.spotifyNoPremium;
+
+      alwaysEnableDevTools = false;
+
+    };
 
 
-  home.packages = [
-    pkgs.spotify
-  ];
 }
