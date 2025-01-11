2752d5fa7e88281a89833557fa3cd67efb62e6b6
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-01-11 00:36:12 +0000

    re-enable macos stuff

--- flake.nix
+++ flake.nix
@@ -13,18 +13,18 @@
 
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-unified.url = "github:srid/nixos-unified";
-    #determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
 
     # Software inputs
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
 
-    #nixvim.url = "github:nix-community/nixvim";
-    #nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    #nixvim.inputs.flake-parts.follows = "flake-parts";
+    nixvim.url = "github:nix-community/nixvim";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
+    nixvim.inputs.flake-parts.follows = "flake-parts";
 
-    #spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    #spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
 
   };
   # Wired using https://nixos-unified.org/autowiring.html
