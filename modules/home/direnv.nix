af0714972083cde81a6275a259330d3591e62da8
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-03-10 11:25:05 -0400

    direnv zsh integration why not

--- modules/home/direnv.nix
+++ modules/home/direnv.nix
@@ -3,9 +3,11 @@
   # https://nixos.asia/en/direnv
   programs.direnv = {
     enable = true;
+    enableZshIntegration = true;
     nix-direnv = {
       enable = true;
     };
+
     config.global = {
       # Make direnv messages less verbose
       hide_env_diff = true;
