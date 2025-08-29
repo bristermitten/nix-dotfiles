26bfab7d29efad08f8acff8caf6eef7601172b35
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-08-29 16:49:05 +0100

    refactor: remove commented-out noice settings from nixvim.nix

--- modules/home/neovim/nixvim.nix
+++ modules/home/neovim/nixvim.nix
@@ -37,8 +37,7 @@
         bottom_search = true;
         command_palette = true;
         long_message_to_split = true;
-        #inc_rename = false;
-        #lsp_doc_border = false;
+        
       };
     };
     telescope = {
