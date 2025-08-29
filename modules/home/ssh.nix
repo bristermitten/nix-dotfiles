7186c756f839bcc86daf21a35703052abcdde156
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-08-29 16:48:33 +0100

    refactor: reformat extraConfig in ssh.nix

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -4,24 +4,22 @@
     includes = [ "config.d/*" ];
     forwardAgent = true;
     extraConfig = ''
-          AddKeysToAgent yes
-          UseKeychain yes
+      AddKeysToAgent yes
+      UseKeychain yes
 
       Host github-uni
-              HostName github.com
-              User fi22264
-              IdentityFile ~/.ssh/id_ed25519_uni
-              IdentitiesOnly=yes
+        HostName github.com
+        User fi22264
+        IdentityFile ~/.ssh/id_ed25519_uni
+        IdentitiesOnly=yes
 
       Host lab
-              HostName rd-mvb-linuxlab.bristol.ac.uk
-              ProxyJump seis
+        HostName rd-mvb-linuxlab.bristol.ac.uk
+        ProxyJump seis
 
       Host seis
-            HostName seis.bris.ac.uk
-            User fi22264
-
-
+        HostName seis.bris.ac.uk
+        User fi22264
     '';
   };
 }
