ec553c422b6f8fb5a2e0f060cf68922411c7a79c
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-09-01 13:34:18 +0100

    manage jdks with nix

---
+++ modules/darwin/jvms.nix
@@ -0,0 +1,27 @@
+{ pkgs, lib, ... }:
+
+let
+  jdkVersions = [ 8 11 17 21 24 ];
+  #  iterate over jdk versions and symlink them to the JavaVirtualMachines directory
+  #  source: https://samasaur1.github.io/blog/jdks-on-nix-darwin
+  makeZuluLinks = { pkgs, lib }: versions:
+    lib.concatStringsSep "\n" (
+      lib.map
+        (version:
+          let
+            versionStr = toString version;
+            jdkPackage = pkgs."jdk${versionStr}";
+            jdkDir = "zulu-${versionStr}.jdk";
+          in
+          ''
+            ln -sf "${jdkPackage}/Library/Java/JavaVirtualMachines/${jdkDir}" "/Library/Java/JavaVirtualMachines/"
+          ''
+        )
+        versions
+    );
+in
+{
+
+  system.activationScripts.extraActivation.text =
+    makeZuluLinks { inherit pkgs lib; } jdkVersions;
+}
