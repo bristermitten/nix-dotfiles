e0726ba7d92b1a85d3468a9413531ccc0d6283e8
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-10-21 15:01:14 +0100

    fix use of deprecated ssh config format

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -2,24 +2,43 @@
   programs.ssh = {
     enable = true;
     includes = [ "config.d/*" ];
-    forwardAgent = true;
-    extraConfig = ''
-      AddKeysToAgent yes
-      UseKeychain yes
+    enableDefaultConfig = false;
 
-      Host github-uni
-        HostName github.com
-        User fi22264
-        IdentityFile ~/.ssh/id_ed25519_uni
-        IdentitiesOnly=yes
+    # default config options for all hosts
+    matchBlocks."*" = {
+      forwardAgent = false;
+      addKeysToAgent = "yes";
+      compression = false;
+      serverAliveInterval = 0;
+      serverAliveCountMax = 3;
+      hashKnownHosts = false;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
 
-      Host lab
-        HostName rd-mvb-linuxlab.bristol.ac.uk
-        ProxyJump seis
+      extraOptions = {
+        UseKeychain = "yes";
+      };
+    };
 
-      Host seis
-        HostName seis.bris.ac.uk
-        User fi22264
-    '';
+    matchBlocks."github-uni" = {
+      hostname = "github.com";
+      user = "git";
+      identityFile = "~/.ssh/id_ed25519_uni";
+      identitiesOnly = true;
+    };
+
+    matchBlocks."lab" = {
+      hostname = "rd-mvb-linuxlab.bristol.ac.uk";
+      proxyJump = "seis";
+      user = "fi22264";
+    };
+
+    matchBlocks."seis" = {
+      hostname = "seis.bris.ac.uk";
+      user = "fi22264";
+      forwardAgent = true;
+    };
   };
 }
